b7dc702b24d95ebab7880ead95f352c160a17a2e
alirnamvar <alirnamvar1999@gmail.com>
2022-11-02 11:58:29 +0330

    update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,5 @@
 from .redis_handler import RedisHandler
 from .order_handler import OrderHandler
+from constants import WAREHOUSE_TABLE_DESCRIPTION, DISASSEMBLE_ORDERS_TABLE_DESCRIPTION
 
 # print('in init')
